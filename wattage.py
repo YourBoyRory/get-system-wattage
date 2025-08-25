265b7f3a9ad5046684c32f6bfa875a64f4ecda22
YourBoyRory <yourboyrory@proton.me>
2025-08-25 13:59:51 -0400

    did stuff

--- wattage.py
+++ wattage.py
@@ -1,10 +1,24 @@
+#!/usr/bin/python3
+
 import sys
+import time
+import argparse
 import os
 sys.path.insert(0, os.path.abspath('Hardware-Monitor/'))
-from HardwareLib import Linux_UPS_Prober 
+from HardwareLib import Linux_UPS_Prober
+
+parser = argparse.ArgumentParser(description="Get Wattage from system.")
+parser.add_argument('-m', '--monitor', action='store_true', help="Enable monitoring mode")
+args = parser.parse_args()
 
 temp = sys.stdout
 sys.stdout = None
 upsProber = Linux_UPS_Prober()
 sys.stdout = temp
+while args.monitor:
+    try:
+        print(upsProber.get_current_load(), "Watts", end='\r')
+        time.sleep(1)
+    except KeyboardInterrupt:
+        break
 print(upsProber.get_current_load(), "Watts")
